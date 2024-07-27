3db0fc9681ea98cb2f1a0567027eea8b191b1494
mykim <minie000309@gmail.com>
2024-07-27 09:04:23 +0900

    chart project

---
+++ Chart/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿/*
+ *   This is free and unencumbered software released into the public domain.
+
+ *   Anyone is free to copy, modify, publish, use, compile, sell, or
+ *   distribute this software, either in source code form or as a compiled
+ *   binary, for any purpose, commercial or non-commercial, and by any
+ *   means.
+
+ *   For more information, please visit https://github.com/hanav00.
+ */
+
+using Chart.ViewModel;
+using System.Windows;
+
+namespace Chart
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+    }
+}
